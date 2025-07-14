e314db6830f89821288b91c85677166c89032d63
Mohamed Iro <68305746+Iro-sources@users.noreply.github.com>
2025-07-14 19:41:52 +0200

    Delete 100Code directory

--- 100Code/Oblig3/Benyttmodul.py
+++
@@ -1,8 +0,0 @@
-
-books = ["The Hobbit", "Farmer", "Giles of Ham", "The Fellowship of the Ring",
-         "The Two Towers", "The Return of the King", "The Adventures of Tom Bombadil", "Tree and Leaf"]
-
-
-
-my_books = []
-

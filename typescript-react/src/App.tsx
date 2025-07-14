4a65f65781c2ef589de0747e9c0596a84cf7ec5a
Iro-sources <mohamediro98@gmail.com>
2025-07-14 20:51:54 +0200

    day1: setup project and basic TypeScript config

---
+++ typescript-react/src/App.tsx
@@ -0,0 +1,113 @@
+import React from 'react';
+import './App.css';
+import InputField from './components/InputFeild';
+
+
+const App: React.FC =() =>{
+  return (
+    <div className="App">
+      <span className="heading">Taskify</span>
+    <InputField/>
+    </div>
+  );
+}
+
+export default App;
+
+/*
+
+//Types of typecript
+let name:string = "mohamed";
+//making age variable take both string and number, using unions
+let age: number | string;
+age = 16;
+age = "sixteen";
+
+let isStudent:boolean;
+let subjects:string[];
+
+//Tupple that takes fast data
+let workPerDay:[string, number];
+workPerDay = ["monday", 7];
+
+//Creating an object
+/* type Person = {
+  name: string
+  //To make the age optional
+  age?: number
+} */
+
+/* let person:Person = {
+  name: "mack",
+}; */
+
+/* let person:Person = {
+  name: "mack",
+  age: 52,
+}; */
+
+//Defining a function
+/* function printName(name: string){
+  console.log(name);
+}
+printName("Mohamed"); */
+
+//let printName: Function;
+//let printName: (name: string) => void;
+//let newFunc: (name: string) => never;
+
+//A function can have different types of return types
+
+//such variable can take any type
+//let personName: unknown;
+//Extending a type
+/* type Fruits = {
+  name: string
+  items: number
+}
+type MyList = Fruits &{
+  name: string
+  item: number
+}
+
+
+interface Student {
+  university: string;
+  year: number; 
+}
+
+interface department extends Student{
+  profession: string
+}
+
+const csDepartment = {
+  university: "Havard",
+  year: 2,
+  profession: "Computer science"
+}
+ */
+// function that takes a student object:
+/* function registerStudent(student:{
+  name:string;
+  age?:number;
+  subjects:string[];
+  studentInfo:department
+
+}): string{
+    return `Student ${student.name} from ${student.studentInfo.university}, 
+    studying ${student.studentInfo.profession}, enrolled in ${student.subjects.length} subjects.`;
+
+}
+
+const studentExample = {
+  name: "Mohamed",
+  age: 28,
+  subjects: ["Parallel programing", "Descreate math", "Algo"],
+  studentInfo:{
+    university: "MIT",
+    year: 2,
+    profession: "Engineering"
+  }
+};
+
+console.log(registerStudent(studentExample)); */
